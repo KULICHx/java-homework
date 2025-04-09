774e8c5fa6845c7f2ded250c86db74c84adf7e57
Vladimir Kulich <vova011004@gmail.com>
2025-04-09 23:04:17 +0400

    fex: Task0 - @SuppressWarnings("uncommentedmain")

--- src/main/java/edu/hw1/Task0.java
+++ src/main/java/edu/hw1/Task0.java
@@ -2,6 +2,7 @@ package edu.hw1;
 
 import java.util.logging.Logger;
 
+@SuppressWarnings("uncommentedmain")
 public final class Task0 {
     private static final Logger LOGGER = Logger.getLogger(Task0.class.getName());
 
@@ -9,9 +10,6 @@ public final class Task0 {
         // Приватный конструктор для утилитного класса
     }
 
-    /**
-     * Entry point of the program. Logs the message "Привет, мир!" using the logger.
-     */
     public static void main(String[] args) {
         LOGGER.info("Привет, мир!");
     }
